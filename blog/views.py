9ffc9eb3f5ca591abe8791f509b130bded52cce4
Rui Meira <rui.meira@revolut.com>
2024-06-19 18:58:27 +0100

    Load posts dinamically

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
+import requests
 from django.shortcuts import render
 
 
+def get_posts_data():
+    data = requests.get("https://jsonplaceholder.typicode.com/posts").json()
+    return data
+
+
 def blog(request):
-    return render(request, "blog/index.html")
+    return render(request, "blog/index.html", {"posts": get_posts_data()})
